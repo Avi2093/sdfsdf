1cf55e5a10e2b66a00e1617e2effcc44a791fb62
Avi2093 <92841507+Avi2093@users.noreply.github.com>
2023-03-07 01:20:36 +0530

    Add files via upload

---
+++ JiraTaskDetail.cs
@@ -0,0 +1,31 @@
+﻿namespace Ticketingtool.Models
+{
+    public class JiraTaskDetail
+    {
+      
+        public string issuetype { get; set; }
+        public string description { get; set; }
+        public string jiraProjectName { get; set; }
+        public string reporter { get; set; }
+
+        public string jiraProjectKey { get; set; }
+        public string LOB { get; set; }
+        public string issueKey { get; set; }
+
+        public string parentKey { get; set; }
+
+
+        public string EpicName { get; set; }
+
+        public string TaskName { get; set; }
+
+        public string StoryDesc { get; set; }
+
+        public string EpicIssueKey { get; set; }
+
+
+
+        // add other properties here
+    }
+
+}
