997e16182bb3d448366657f5cbd620228c026099
Avi2093 <92841507+Avi2093@users.noreply.github.com>
2023-03-09 12:19:10 +0530

    Add files via upload

---
+++ RequestTaskDetails.cs
@@ -0,0 +1,21 @@
+﻿namespace Ticketingtool.Models
+{
+    public class RequestTaskDetails
+    {
+        public string projectJiraKey { get; set; }
+
+        public string epicIssueKey { get; set; }
+
+        public string taskType { get; set; }
+
+        public string issueKey { get; set; }
+
+        public string summary { get; set; }
+
+        public string description { get; set; }
+
+        public string employeeEmail { get; set; }
+
+
+    }
+}
---
+++ xref_productdevelopment_team.cs
@@ -0,0 +1,18 @@
+﻿namespace Ticketingtool.Models
+{
+    public class xref_productdevelopment_team
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string employeeEmail { get; set; }
+
+        public int active { get; set; }
+        
+
+
+
+
+
+
+    }
+}
