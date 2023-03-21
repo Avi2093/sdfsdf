5dda9b79ec5b6ead3b93323a6fd5e0c3cc45bf9d
Avi2093 <92841507+Avi2093@users.noreply.github.com>
2023-03-21 12:57:14 +0530

    Update ErrorViewModel.cs

--- ErrorViewModel.cs
+++ ErrorViewModel.cs
@@ -1,12 +1,50 @@
-namespace Ticketingtool.Models
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
 {
-    public class ErrorViewModel
+    public class RegisterContoller : Controller
     {
-        public string RequestId { get; set; }
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        [HttpPost]
+        public IActionResult Index(Writer writer, string passwordAgain)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(writer);
+            if (results.IsValid && writer.WriterPassword == passwordAgain)
+            {
+                writer.WriterStatus = true;
+                writer.WriterAbout = "Deneme test";
+                writerManager.WriterAdd(writer);
+                return RedirectToAction("Index", "Blog");
+            }
+            else if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("WriterPassword", "Girdiğiniz Şifreler Eşleşmedi Lütfen Tekrar Deneyin");
+            }
+            return View();
+        }
 
-        public string Message { get; set; }
     }
-
 }
