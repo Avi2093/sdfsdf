cf43bd5f666c216860b72d0ba0cf0492b3eac3ab
Earner Borgs <92841507+Avi2093@users.noreply.github.com>
2024-04-26 16:45:38 +0530

    Update ErrorViewModel.cs

--- ErrorViewModel.cs
+++ ErrorViewModel.cs
@@ -1,1 +1,5 @@
-hgjhgjhgjh
+curl.exe -X POST https://api.twilio.com/2010-04-01/Accounts/AC730ea004ff6c24/Calls.json ^
+  --data-urlencode "Url=http://demo.twilio.com/docs/voice.xml" ^
+  --data-urlencode "To=+918319338" ^
+  --data-urlencode "From=+12512011" ^
+  -u "A:"
